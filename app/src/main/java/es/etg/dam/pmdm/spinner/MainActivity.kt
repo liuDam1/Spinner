5801d85cd65ab9bbb411a673f306967705dfc0a0
liuDam1 <liu.jiangyu.dam@gmail.com>
2025-11-11 17:20:55 +0100

    Cargar datos con ListView

--- app/src/main/java/es/etg/dam/pmdm/spinner/MainActivity.kt
+++ app/src/main/java/es/etg/dam/pmdm/spinner/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.ListView
 import android.widget.Spinner
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
@@ -16,10 +17,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        // Parte de Spinner
         val sp: Spinner = findViewById(R.id.spinner)
         sp.adapter = getAdapter()
-
         sp.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+            }
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                Toast.makeText(view?.context, "Seleccionado $position", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        // Parte de ListView
+        val lv: ListView = findViewById(R.id.listView)
+        lv.adapter = getAdapter()
+        lv.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
             override fun onNothingSelected(parent: AdapterView<*>?) {
             }
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
@@ -28,6 +41,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    // Funcion para cargar las listas usando XML
     fun getAdapter(): ArrayAdapter<CharSequence> {
         val adapter = ArrayAdapter.createFromResource(this,
             R.array.lista_ciudades,
@@ -35,6 +49,7 @@ class MainActivity : AppCompatActivity() {
         return adapter;
     }
 
+    // Funcion para cargar las listas usando Array
 //    fun getAdapter(): ArrayAdapter<String> {
 //        val ciudades = arrayOf("Parla", "Barcelona", "Madrid")
 //        val adapter = ArrayAdapter(this,android.R.layout.simple_spinner_item,ciudades)
